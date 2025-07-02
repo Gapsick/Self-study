bec2d6ec4e30a433408a04d4f2cf1862143a6966
Gapsick <gapsicke@gmail.com>
2025-07-02 23:54:49 +0900

    add part4_ch1 about js

---
+++ Fast_campus/part4_ch1/main.js
@@ -0,0 +1,17 @@
+// const user = new Object()
+// user.name = 'Seongsik'
+// user.age = 80
+
+// function User() {
+//     this.name = 'KIM',
+//     this.age = 85
+// }
+
+// const user = new User()
+
+const user = {
+    name: "KIM",
+    age: 85
+}
+
+console.log(user)
--- Fast_campus/part4_ch1/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'test/plain'
-    });
-    res.end('Hello, world');
-})
-
-server.listen(4000);
