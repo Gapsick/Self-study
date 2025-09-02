0ee5081218d977c2fcf0831f785c352a0ae22c74
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-09-02 23:56:29 +0900

    add: Study React

---
+++ React/my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  const name = "Seongsik";
+  const naver = {
+    name: "네이버",
+    url: "https://naver.com"
+  }
+  return (
+    <div className="App">
+      <h1 style={{
+        color: "#f0f",
+        backgroundColor: "green",
+      }}
+    >
+      Hello, {name}.<p>{2 + 3}</p>
+      </h1>
+      <a href={naver.url}>{naver.name}</a>
+    </div>
+  )
+}
+
+export default App;
