47b174a3b47a72873c31f6a12175306c6a4f8fe3
Gapsick <gapsicke@gmail.com>
2025-10-26 21:18:05 +0900

    pi7번 사진 저장 수정

--- Parking-control-system-Backend/src/sockets/clientManager.js
+++ Parking-control-system-Backend/src/sockets/clientManager.js
@@ -16,6 +16,33 @@ export default (io) => {
         console.log(`등록 완료 → ${id} (${socket.id})`);
         });
 
+        // pi7번으로부터 사진 받기
+        socket.on("entry_photo", async (data) => {
+            const { car_number, image } = data
+            if (!car_number || !image) return;
+
+            try {
+                const fileName = `${Date.now()}_${car_number}.jpg`;
+                const uploadDir = path.join(process.cwd(), "uploads/cars");
+                if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
+                const filePath = path.join(uploadDir, fileName);
+
+                fs.writeFileSync(filePath, Buffer.from(image.replace(/^data:image\/\w+;base64,/, ""), "base64"));
+
+                await pool.query(
+                    `UPDATE parking_event 
+                    SET entry_photo_url=? 
+                    WHERE plate_number=? AND exit_time IS NULL
+                    ORDER BY id DESC LIMIT 1`,
+                    [`/uploads/cars/${fileName}`, car_number]
+                );
+
+                console.log(`[입구 사진 저장 완료] ${car_number}`);
+            } catch (err) {
+                console.error("사진 저장 오류:", err.message);
+            }
+        })
+
         // 연결 해제 시 목록에서 제거
         socket.on("disconnect", () => {
         const disconnectedId = Object.keys(connectedClients).find(
